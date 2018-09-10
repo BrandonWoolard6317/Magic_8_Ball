fa8e94c4eb4d9edee8fc5bbfbf0f53567a75be35
Brandon Woolard <42773989+BrandonWoolard6317@users.noreply.github.com>
2018-09-10 10:36:50 -0400

    Creates if statements

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -3,19 +3,52 @@ import java.util.Scanner;
 public class Magic8Ball {
 
     public static void main(String[] args){
-        String Questioning;
-        int Testing;
+        String usersInput;
+        int Questioning;
 
         Scanner keyboard = new Scanner(System.in);
 
         System.out.println("Let's play Magic 8 Ball!\nPick a number between 1-100");
-        Questioning = keyboard.nextLine();
-
-    if(Questioning <= 10){
-
-    }
-
-
+        usersInput = keyboard.nextLine();
+        
+        usersInput = Integer.parseInt(String.valueOf(Questioning));
+        if(Questioning == 0){
+            System.out.println("Too low of a number!")
+        }
+        else if(Questioning == 1-10){
+            System.out.println("");
+        }
+        else if(Questioning == 11-20){
+            System.out.println("");
+        }
+        else if(Questioning == 21-30){
+            System.out.println("");
+        }       
+        else if(Questioning == 31-40){
+            System.out.println("");
+        }        
+        else if(Questioning == 41-50){
+            System.out.println("");
+        }      
+        else if(Questioning == 51-60){
+            System.out.println("");
+        }       
+        else if(Questioning == 61-70){
+            System.out.println("");
+        }       
+        else if(Questioning == 71-80){
+            System.out.println("");
+        }      
+        else if(Questioning == 81-90){
+            System.out.println("");
+        }      
+        else if(Questioning == 91-100){
+            System.out.println("");
+        }
+        else if(Question > 100){
+            System.out.println("Too high of a number!")
+        }
+        
 
 
 
