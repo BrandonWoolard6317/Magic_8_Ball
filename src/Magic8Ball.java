bbe1552b841f17a650a84833bf31868b063dc7bd
Brandon Woolard <brandon.woolard.2019@student.lhschools.org>
2018-09-10 09:45:26 -0400

    Started Project

---
+++ src/Magic8Ball.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Magic8Ball {
+
+    public static void main(String[] args){
+        Scanner keyboard;
+        String Questioning;
+        int Testing;
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
