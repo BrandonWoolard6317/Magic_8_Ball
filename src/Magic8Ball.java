32c1eb42437492bb5c02023bb0a4a1d9241a003d
Brandon Woolard <brandon.woolard.2019@student.lhschools.org>
2018-09-10 10:02:49 -0400

    Finished initializing

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -3,11 +3,17 @@ import java.util.Scanner;
 public class Magic8Ball {
 
     public static void main(String[] args){
-        Scanner keyboard;
         String Questioning;
         int Testing;
 
-        
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Let's play Magic 8 Ball!\nPick a number between 1-100");
+        Questioning = keyboard.nextLine();
+
+    if(Questioning <= 10){
+
+    }
 
 
 
