01a2b5c2eeb755d79cb84a5c4b11aabc6dab2669
Brandon Woolard <42773989+BrandonWoolard6317@users.noreply.github.com>
2018-09-25 12:09:52 -0400

    a

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -7,16 +7,68 @@ public class Magic8Ball {
         String usersInput;
         int generatedNumber;
         int Questioning;
-        int usersNumber;
 
         //Initializing Variables
         Scanner keyboard = new Scanner(System.in);
         generatedNumber = (int) (Math.random() * 100);
 
+        
+        
         System.out.println("Lets play Magic 8 Ball! Select a number between 0-100");
         usersInput = keyboard.nextLine();
-        usersAnswer = Integer.parseInt(String.valueof(usersInput));
+        Questioning = Integer.parseInt(String.valueof(usersInput));
         
+        if(Questioning <= 0){
+             System.out.println("Too low of a number!");
+         }
+         else if(Questioning <= 10){
+         else if(Questioning < 10){
+             System.out.println("You will be surrounded by loved ones your entire life.");
+         }
+         else if(Questioning >= 11){
+         else if(Questioning < 20){
+             System.out.println("You will always be alone.");
+         }
+         else if(Questioning >= 21-30){
+         else if(Questioning < 30){
+             System.out.println("You will have endless amounts of money.");
+         }       
+         else if(Questioning == 31-40){
+         else if(Questioning < 40){
+             System.out.println("You will never make it above medium class in wealth.");
+         }        
+         else if(Questioning == 41-50){
+         else if(Questioning < 50){
+             System.out.println("You will have bad luck for the rest of your life.");
+         }      
+         else if(Questioning == 51-60){
+         else if(Questioning < 60){
+             System.out.println("You will have many great things coming soon.");
+         }       
+         else if(Questioning == 61-70){
+         else if(Questioning < 70){
+             System.out.println("You will never be ill.");
+         }       
+         else if(Questioning == 71-80){
+         else if(Questioning < 80){
+             System.out.println("You will have bad fortune for many years to come.");
+         }      
+         else if(Questioning == 81-90){
+         else if(Questioning < 90){
+             System.out.println("You will have great fortune.");
+         }      
+         else if(Questioning == 91-100){
+         else if(Questioning <= 100){
+             System.out.println("You are not awesome.");
+         }
+         else if(Questioning > 100){
+         else if(Questioning >= 101){
+             System.out.println("Too high of a number!");
+         }
+        
+             
+             
+             
         /*Loop 
         System.out.println("Let's play Magic 8 Ball!\nPress 1 to play\nPress 2 to exit");
         usersInput = keyboard.nextLine();
@@ -81,5 +133,7 @@ public class Magic8Ball {
                 System.exit(0);
             }
         }*/
+             
+             
     }
 }
